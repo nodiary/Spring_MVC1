9d7f526eaa4dcb6a90525a22d33e047149bf7ebb
roh <sunlitground@gmail.com>
2024-04-22 22:41:14 +0900

    section 3 - MVC패턴 적용

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.servletmvc;
+
+import hello.domain.member.Member;
+import hello.domain.member.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servle-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        req.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+
+    }
+}
